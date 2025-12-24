4ded9be6e7cb06feb439f176ab3ad6111d0ed56c
Zehavi <ze8544@gmail.com>
2025-12-24 22:39:54 +0200

    24.12.25

--- Saled/obj/Debug/net8.0/1.AssemblyInfo.cs
+++ Saled/obj/Debug/net8.0/1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ecc5939b51ab934bbb03710562b4af4a3fbfc99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+493052d9d242bf3c4be49ad91ad003241cf2110a")]
 [assembly: System.Reflection.AssemblyProductAttribute("1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
