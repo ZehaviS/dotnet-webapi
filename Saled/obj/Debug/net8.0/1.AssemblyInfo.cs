6646b55b5c8451ae95b2aa90a0f4f83edf65de4f
ZehaviS <ze8544@gmail.com>
2026-03-17 14:48:33 +0200

    fix: enforce user update authorization and prevent clearance escalation

--- Saled/obj/Debug/net8.0/1.AssemblyInfo.cs
+++ Saled/obj/Debug/net8.0/1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6bc3012aeb8dafad52215bbd0f9c950c74ab066")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85d27bf29dcba541a9c3a191d84195a66243f509")]
 [assembly: System.Reflection.AssemblyProductAttribute("1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
