ff3cc65e7946c8cd49ef51db33abc4390090d959
ZehaviS <ze8544@gmail.com>
2026-05-13 17:28:20 +0300

    add README

--- Saled/obj/Debug/net8.0/1.AssemblyInfo.cs
+++ Saled/obj/Debug/net8.0/1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6bc3012aeb8dafad52215bbd0f9c950c74ab066")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+976016f34a3922417f48a73afd1b104befc79948")]
 [assembly: System.Reflection.AssemblyProductAttribute("1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
